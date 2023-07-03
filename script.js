88d768cb81175b6f86a0031acf43907ad7a1c33e
Manoj Saini <manojsaini513@gmail.com>
2023-07-03 10:11:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,31 +2,31 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-let a = new Promise(resolve => {
+let a = new Promise((resolve,reject) => {
 	setTimeout(() => {
 		resolve("a")
 	}, 3000)
 })
 
-let b = new Promise(resolve => {
+let b = new Promise((resolve,reject) => {
 	setTimeout(() => {
 		resolve("b")
 	}, 2000)
 })
 
-let c = new Promise(resolve => {
+let c = new Promise((resolve,reject) => {
 	setTimeout(() => {
 		resolve("c")
 	}, 1000)
 })
 
-let d = new Promise(resolve => {
+let d = new Promise((resolve,reject) => {
 	setTimeout(() => {
 		resolve("d")
 	}, 4000)
 })
 
-let e = new Promise(resolve => {
+let e = new Promise((resolve,reject) => {
 	setTimeout(() => {
 		resolve("e")
 	}, 5000)
